9f1588ceafb44d515440d486a039277d5a7eceaf
Vlad Pastushenko <vladp@ro.ru>
2020-11-25 10:52:54 +0300

    Refactoring wiki bot

--- bot.py
+++ bot.py
@@ -1,91 +1,102 @@
-# import нужных библиотек
-import telebot
-# import config
-import wikipedia
+import os
 
+import telebot
+import wikipedia
+from dotenv import load_dotenv
 from telebot import types
 
-# объявляем переменные
-search = "📱Поиск информации📱"
-about = "❗️О сетке ботов inedubots❗️"
-support = "💸Поддержать💸"
-rus = "🔴Русский язык поиска🔴"
-eng = "🟠Английский язык поиска🟠"
-enter = "🔍Введите запрос для поиска🔍"
-about_reaction = "❗️В сетку ботов входят боты: @rubdollarbot, @pernamebot, @wikiinedubot, @mathinedubot❗️ \n❗️Сетка ботов inedubots создана для привлечения инвестиций к боту @RuHelpEducationBot❗️"
-diamond = "💎5599 0050 4579 2269💎"
-set_red = "🔴Установлено🔴"
-set_orange = "🟠Установлено🟠"
+load_dotenv()
+client = telebot.TeleBot(os.getenv('BOT_TOKEN'))
 
-# устанавливаем русский язык поиска (по умолчанию)
-wikipedia.set_lang("ru")
+SEARCH_BUTTON = '📱Поиск информации📱'
+ABOUT_BUTTON = '❗️О сетке ботов inedubots❗️'
+SUPPORT_BUTTON = '💸Поддержать💸'
+RUS_BUTTON = '🔴Русский язык поиска🔴'
+ENG_BUTTON = '🟠Английский язык поиска🟠'
 
-# берём токен из файла
-client = telebot.TeleBot("1358011014:AAGa1B9724IRJhIxaiBKUYXKqwEpOkDC6ws")
+HELLO_MESSAGE = 'Добро пожаловать, {0.first_name}!\n' \
+                'Я - <b>{1.first_name}</b>, бот созданный MIKHAN_GO для поиска информации в википедии)'
+ENTER_QUERY_MESSAGE = '🔍Введите запрос для поиска🔍'
+CHANGE_LANGUAGE_MESSAGE = '🔴Смените язык🔴'
+BAD_WIKI_REQUEST_MESSAGE = '🔴Такого запроса нету в википедии!🔴'
+ABOUT_MESSAGE = '❗️В сетку ботов входят боты: @rubdollarbot, @pernamebot, @wikiinedubot, @mathinedubot❗️\n' \
+                '❗️Сетка ботов inedubots создана для привлечения инвестиций к боту @RuHelpEducationBot❗️'
+CARD_NUMBER_MESSAGE = '💎5599 0050 4579 2269💎'
+RED_SET_MESSAGE = '🔴Установлено🔴'
+ORANGE_SET_MESSAGE = '🟠Установлено🟠'
+BAD_REQUEST_MESSAGE = '🔴Несуществущий запрос!🔴'
 
-# команда старта бота
-@client.message_handler(commands = ['start'])
-def hello(message):
-    # отправляем стикер
-    hello = open('static/welcome.webp', 'rb')
-    client.send_sticker(message.chat.id, hello)
 
-    # добавляем клавиатуру к сообщению
+@client.message_handler(commands=['start'])
+def start_handler(message):
+    # hello = open('static/welcome.webp', 'rb')
+    # client.send_sticker(message.chat.id, hello)
+
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    search = types.KeyboardButton(search)
-    about = types.KeyboardButton(about)
-    support = types.KeyboardButton(support)
-    rus = types.KeyboardButton(rus)
-    eng = types.KeyboardButton(eng)
+    markup.add(
+        types.KeyboardButton(SEARCH_BUTTON),
+        types.KeyboardButton(ABOUT_BUTTON),
+        types.KeyboardButton(SUPPORT_BUTTON),
+        types.KeyboardButton(RUS_BUTTON),
+        types.KeyboardButton(ENG_BUTTON),
+    )
 
-    markup.add(search, about, support, rus, eng)
+    client.send_message(
+        message.chat.id,
+        HELLO_MESSAGE.format(message.from_user, client.get_me()),
+        parse_mode='html',
+        reply_markup=markup,
+    )
 
-    # отправляем сообщение с прикреплённой клавиатурой, приветствуя пользователя по никнейму
-    client.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный MIKHAN_GO для поиска информации в википедии)".format(message.from_user,
-    client.get_me()), parse_mode='html', reply_markup=markup)
 
-def search_wikipedia(message):
-    # выводим запрос поиска в википедии
+@client.message_handler(regexp=SEARCH_BUTTON)
+def start_search_handler(message):
+    client.send_message(message.chat.id, ENTER_QUERY_MESSAGE)
+    client.register_next_step_handler(message, search_query_handler)
+
+
+def search_query_handler(message):
     try:
-        client.send_message(message.chat.id, "Вот результат поиска вашего запроса в википедии: " + wikipedia.summary(message.text))
-    # иначе если ошибка подключения пишем о смене языка
+        client.send_message(
+            message.chat.id,
+            'Вот результат поиска вашего запроса в википедии: '
+            + wikipedia.summary(message.text),
+        )
+
     except ConnectionError:
-        client.send_message(message.chat.id, "🔴Смените язык🔴")
+        client.send_message(message.chat.id, CHANGE_LANGUAGE_MESSAGE)
+
     except:
-        client.send_message(message.chat.id, "🔴Такого запроса нету в википедии!🔴")
+        client.send_message(message.chat.id, BAD_WIKI_REQUEST_MESSAGE)
 
-# если всё хорошо
-try:
-    # search
-    @client.message_handler(regexp=search)
-    def search(message):
-        client.send_message(message.chat.id, enter)
-        client.register_next_step_handler(message, search_wikipedia)
 
-    # about
-    @client.message_handler(regexp=search)
-    def about(message):
-        client.send_message(message.chat.id, about_reaction)
+@client.message_handler(regexp=ABOUT_BUTTON)
+def about_handler(message):
+    client.send_message(message.chat.id, ABOUT_MESSAGE)
 
-    # support
-    @client.message_handler(regexp=support)
-    def about(message):
-        client.send_message(message.chat.id, diamond)
 
-    # rus
-    @client.message_handler(regexp=search)
-    def about(message):
-        client.send_message(message.chat.id, set_red)
-        wikipedia.set_lang("ru")
+@client.message_handler(regexp=SUPPORT_BUTTON)
+def support_handler(message):
+    client.send_message(message.chat.id, CARD_NUMBER_MESSAGE)
 
-    #eng
-    @client.message_handler(regexp=search)
-    def about(message):
-        client.send_message(message.chat.id, set_orange)
-        wikipedia.set_lang("en")
 
-except:
-    client.send_message(message.chat.id, "🔴Несуществущий запрос!🔴")
+@client.message_handler(regexp=RUS_BUTTON)
+def rus_handler(message):
+    client.send_message(message.chat.id, RED_SET_MESSAGE)
+    wikipedia.set_lang('ru')
 
-# запускаем бота
-client.polling(none_stop = True, interval = 0)
+
+@client.message_handler(regexp=ENG_BUTTON)
+def eng_handler(message):
+    client.send_message(message.chat.id, ORANGE_SET_MESSAGE)
+    wikipedia.set_lang('en')
+
+
+@client.message_handler(content_types=['text'])
+def fallback(message):
+    client.send_message(message.chat.id, BAD_REQUEST_MESSAGE)
+
+
+if __name__ == '__main__':
+    wikipedia.set_lang('ru')
+    client.polling(none_stop=True, interval=0)
